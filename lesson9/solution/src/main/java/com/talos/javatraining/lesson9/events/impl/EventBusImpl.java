b2196f6f677b49ea1125e47bf5725818b3f7b965
Alejandro Hernandez Valencia <alejo.web.dev@gmail.com>
2018-12-05 15:24:26 -0500

    Replaced occurrences of CalculatorEvent with Consumer interface. Then deleted the CalculatorEvent interface.

--- lesson9/solution/src/main/java/com/talos/javatraining/lesson9/events/impl/EventBusImpl.java
+++ lesson9/solution/src/main/java/com/talos/javatraining/lesson9/events/impl/EventBusImpl.java
@@ -1,6 +1,5 @@
 package com.talos.javatraining.lesson9.events.impl;
 
-import com.talos.javatraining.lesson9.events.CalculatorEvent;
 import com.talos.javatraining.lesson9.events.EventBus;
 import com.talos.javatraining.lesson9.events.EventType;
 
@@ -9,11 +8,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 
 
 public class EventBusImpl implements EventBus
 {
-	private Map<EventType, List<CalculatorEvent>> records;
+	private Map<EventType, List<Consumer>> records;
 
 	public EventBusImpl()
 	{
@@ -21,7 +21,7 @@ public class EventBusImpl implements EventBus
 	}
 
 	@Override
-	public void register(EventType event, CalculatorEvent observer)
+	public void register(EventType event, Consumer observer)
 	{
 		records.computeIfAbsent(event, e -> new ArrayList<>()).add(observer);
 	}
@@ -29,6 +29,6 @@ public class EventBusImpl implements EventBus
 	@Override
 	public void notify(EventType event, String... args)
 	{
-		records.getOrDefault(event, Collections.emptyList()).forEach(o -> o.update(args));
+		records.getOrDefault(event, Collections.emptyList()).forEach(o -> o.accept(args));
 	}
 }
